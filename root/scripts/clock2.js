f45a886fcd37a60123e74c5fed40af30896c61ad
RodikYermak <rodion.yermakov.sd@gmail.com>
2019-03-13 09:44:21 -0700

    clock end

--- root/scripts/clock2.js
+++ root/scripts/clock2.js
@@ -4,20 +4,21 @@ const HOURHAND = document.querySelector('#hour');
 const MINUTEHAND = document.querySelector('#minute');
 const SECONDHAND = document.querySelector('#second');
 
-function clockGo(){
     var date = new Date();
-//    console.log(date);
 
     let hr = date.getHours();
     let min = date.getMinutes();
     let sec = date.getSeconds();
-//    console.log('Hour: ' + hr + ' Minutes: ' + min + ' Seconds: ' + sec);
 
     let hrPos = (hr * 360/12) + ((min*6)/12);
     let minPos = (min * 6) + (sec*(6/60));
     let secPos = sec * 6; // 360/60
-//    console.log(secPos);
 
+function clockGo(){
+    secPos = secPos + 6;
+    minPos = minPos;
+    hrPos = hrPos;
+    
     HOURHAND.style.transform = 'rotate(' + hrPos + 'deg)'; 
     MINUTEHAND.style.transform = 'rotate(' + minPos + 'deg)'; 
     SECONDHAND.style.transform = 'rotate(' + secPos + 'deg)'; 
@@ -26,3 +27,7 @@ function clockGo(){
 
 var myInterval = setInterval(clockGo, 1000);
 // this js works fine without the transition property on the hr, min, sec id's.
+function curTime(){
+    console.log(hr + ' ' + min + ' ' + sec);
+}
+var displayTime = setInterval(curTime, 1000);
